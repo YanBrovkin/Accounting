7b04294710f5116d7d2d403a331c04417e28946a
Yan Brovkin <brovkin.yan@gmail.com>
2017-08-08 18:15:53 +0200

    Library KnockoutJs was updated. Some app skeleton updated also

--- WebApp/Controllers/PricesController.cs
+++ WebApp/Controllers/PricesController.cs
@@ -1,124 +1,13 @@
-﻿using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web.Mvc;
-using DAL.DTO;
-using WebApp.Models;
+﻿using System.Web.Mvc;
 
 namespace WebApp.Controllers
 {
     public class PricesController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
-
         // GET: Prices
         public ActionResult Index()
         {
-            return View(db.PriceDtoes.ToList());
-        }
-
-        // GET: Prices/Details/5
-        public ActionResult Details(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PriceDto priceDto = db.PriceDtoes.Find(id);
-            if (priceDto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(priceDto);
-        }
-
-        // GET: Prices/Create
-        public ActionResult Create()
-        {
             return View();
-        }
-
-        // POST: Prices/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Title,Value")] PriceDto priceDto)
-        {
-            if (ModelState.IsValid)
-            {
-                db.PriceDtoes.Add(priceDto);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(priceDto);
-        }
-
-        // GET: Prices/Edit/5
-        public ActionResult Edit(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PriceDto priceDto = db.PriceDtoes.Find(id);
-            if (priceDto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(priceDto);
-        }
-
-        // POST: Prices/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Title,Value")] PriceDto priceDto)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(priceDto).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(priceDto);
-        }
-
-        // GET: Prices/Delete/5
-        public ActionResult Delete(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PriceDto priceDto = db.PriceDtoes.Find(id);
-            if (priceDto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(priceDto);
-        }
-
-        // POST: Prices/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(long id)
-        {
-            PriceDto priceDto = db.PriceDtoes.Find(id);
-            db.PriceDtoes.Remove(priceDto);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
---
+++ WebApp/Controllers/PricesWebController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Results;
+using DAL.DTO;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class PricesWebController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Web
+        public JsonResult<List<PriceDto>> GetAllPrices()
+        {
+            return Json(db.PriceDtoes.ToList());
+        }
+
+        // GET: api/Web/5
+        [ResponseType(typeof(PriceDto))]
+        public async Task<IHttpActionResult> GetPriceDto(long id)
+        {
+            PriceDto priceDto = await db.PriceDtoes.FindAsync(id);
+            if (priceDto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(priceDto);
+        }
+
+        // PUT: api/Web/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPriceDto(long id, PriceDto priceDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != priceDto.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(priceDto).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PriceDtoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Web
+        [ResponseType(typeof(PriceDto))]
+        public async Task<IHttpActionResult> PostPriceDto(PriceDto priceDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.PriceDtoes.Add(priceDto);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { Id = priceDto.Id }, priceDto);
+        }
+
+        // DELETE: api/Web/5
+        [ResponseType(typeof(PriceDto))]
+        public async Task<IHttpActionResult> DeletePriceDto(long id)
+        {
+            PriceDto priceDto = await db.PriceDtoes.FindAsync(id);
+            if (priceDto == null)
+            {
+                return NotFound();
+            }
+
+            db.PriceDtoes.Remove(priceDto);
+            await db.SaveChangesAsync();
+
+            return Ok(priceDto);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PriceDtoExists(long id)
+        {
+            return db.PriceDtoes.Count(e => e.Id == id) > 0;
+        }
+    }
+}
