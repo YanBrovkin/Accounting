816bc5989d7eec65fb5644c0ca8a97ea226038b2
Yan Brovkin <brovkin.yan@gmail.com>
2017-08-30 18:51:00 +0200

    Start refactoring view - using AngularJs

--- WebApp/Scripts/AccountingClient.js
+++ WebApp/Scripts/AccountingClient.js
@@ -1,4 +1,27 @@
 ﻿var AccountingApp = angular.module('AccountingApp', [])
-AccountingApp.controller('PriceController', function ($scope) {
-    $scope.message = "Infrgistics";
+AccountingApp.controller('PricesController', function ($scope, PricesService) {
+    var vm = this;
+    vm.prices = [];
+
+    getAllPrices();
+
+    function getAllPrices() {
+        PricesService.getAllPrices()
+            .then(function (response) {
+                vm.prices = response.data;
+                console.log($scope.prices);
+            });
+        //.error(function (error) {
+        //    $scope.status = 'Unable to load customer data: ' + error.message;
+        //    console.log($scope.status);
+        //});
+    }
 });
+
+AccountingApp.factory('PricesService', ['$http', function ($http) {
+    var PricesService = {};
+    PricesService.getAllPrices = function () {
+        return $http.get('/api/pricesweb');
+    };
+    return PricesService;
+}]);
